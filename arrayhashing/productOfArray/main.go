d72459825da782e3989f2651bdb2618c24b961df
Олег Мальцев <oyumaltsev@sberbank.ru>
2024-05-28 14:22:20 +0300

    ADD day 7 v2 best version

--- arrayhashing/productOfArray/main.go
+++ arrayhashing/productOfArray/main.go
@@ -6,51 +6,23 @@ import (
 
 func main(){
  	nums := []int{4,3,2,1,2}
-	fmt.Println(countPrefixArray(nums))
-	fmt.Println(countPostfixArray(nums))
 	fmt.Println(productExceptSelf(nums))
 }
 
 func productExceptSelf(nums []int) []int{
-	var resultArr []int
-	arraySize := len(nums)
-	var result int
-	
-	prefixArray := countPrefixArray(nums)
-	postfixArray := countPostfixArray(nums)
+	result := make([]int, len(nums))
 
-	for i := range nums{
-		if i == 0{
-			result = postfixArray[arraySize - 2]
-		} else  if i == arraySize - 1 {
-			result = prefixArray[i - 1]
-		} else {
-			result = prefixArray[i - 1] * postfixArray[arraySize - 2 -i]
-		}
-		resultArr = append(resultArr, result)
-	}
-
-	return resultArr
-}
-
-func countPrefixArray(nums []int) []int{
 	prefix := 1
-
-	var prefixArray []int
-	for _, element := range nums{
-		prefix = prefix * element
-		prefixArray = append(prefixArray, prefix)
+	for i, element := range nums{
+		result[i] = prefix
+		prefix *= element
 	}
-	return prefixArray
-}
 
-func countPostfixArray(nums []int) []int{
 	postfix := 1
-	arraySize := len(nums)
-	var postfixArray []int
-	for i := range nums{
-		postfix = postfix * nums[arraySize - 1 - i]
-		postfixArray = append(postfixArray, postfix)
+	for i := len(nums) - 1; i >= 0; i-- {
+		result[i] *= postfix
+		postfix *= nums[i]
 	}
-	return postfixArray
+	return result
 }
+
